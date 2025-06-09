f64922d5cabc9b4dcc7fc9043fab2a512d392ef7
zt <lzttzllzttzl@gmail.com>
2025-06-09 17:44:12 +0800

    working carpark distance with display

--- Front End/components/CarparkList.tsx
+++ Front End/components/CarparkList.tsx
@@ -1,61 +1,36 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { FlatList, StyleSheet , View, Text} from 'react-native';
 import Button from '@/components/Button';
 import { Region } from 'react-native-maps';
+import CarparkItem from './CarparkItem';
 
 type CarparkListProps = {
   carparks: Carpark[];
   onItemPress: (carpark: Carpark) => void;
-  origin: Region
+  origin?: Region
 }
 
-const getDistance = async (carpark: Carpark, origin : Region) => {
-  try {
-    let response = await fetch("http://192.168.68.60:3000/computeDistance", {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        origin: {
-          latitude: origin.latitude,
-          longitude: origin.longitude
-        },
-        destination: {
-          latitude: carpark.latitude,
-          longitude: carpark.longitude
-        }
-      })
-    })
-    if (!response.ok) throw new Error("tf")
 
+const CarparkList = ({ carparks, onItemPress, origin }: CarparkListProps) => {
+  
+  return (
+    <FlatList
+      data={carparks}
+      renderItem={({ item: carpark }) => (
+        <CarparkItem
+          carpark = {carpark}
+          onPress={onItemPress}
+          origin={origin}>
 
-  } catch (error) {
+        </CarparkItem>
+      )}
+      keyExtractor={(item) => item.id.toString()}
+      style={styles.list}
+      contentContainerStyle={styles.content}
+    />
+  );
+};
 
-  }
-}
-
-const CarparkList = ({ carparks, onItemPress, origin }: CarparkListProps) => (
-  <FlatList
-    data={carparks}
-    //renaming item into carpark
-    renderItem={({ item: carpark }) => (
-      <View>
-        <Button
-          label={carpark.name}
-          onPress={() => onItemPress(carpark)}>
-        </Button>
-        <Text>
-          dsnmfsd
-        </Text>
-      </View>
-
-    )}
-    keyExtractor={(item) => item.id.toString()}
-    style={styles.list}
-    contentContainerStyle={styles.content}
-  />
-);
 
 const styles = StyleSheet.create({
   list: {
