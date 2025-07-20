35f5529299e0575356ac0bbef982b6646ea57437
mackerelsoup <lzttzllzttzl@gmail.com>
2025-07-20 22:33:24 +0800

    minor bug fixes and added custom dropdown componnet

---
+++ Front End/app/approval.tsx
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function approval() {
+  return (
+    <View>
+      <Text>approval</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
